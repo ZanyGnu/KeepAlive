011e2c6b63d7209c9d6368da9b717cd5245be79b
ZanyGnu <ajaymartin@gmail.com>
2014-12-29 16:11:39 -0800

    refactored files for single instance and thread execution status

---
+++ ApplicationHelper.cs
@@ -0,0 +1,78 @@
+﻿
+namespace KeepAlive
+{
+    using Microsoft.Win32;
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+
+    public class CurrentApplication
+    {
+        private static string UniqueBackgroundEnvName = "Env-C72EDBDD-0109-4989-8A08-FDC8E90079EB";
+        private static string UniqueBackgroundEnvValue = "65BAF29A-4E4A-498D-A0A3-F5091CB71104";
+
+        /// <summary>
+        /// Calling this routine ensures that this program is automatically run when the current user logs back in.
+        /// This will thus not require the user to re-run the program.
+        /// </summary>
+        public static void MakeProgramAutoRun()
+        {
+            RegistryKey add = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            add.SetValue("Keep Machine Alive", "\"" + CurrentApplication.GetCurrentApplication() + "\"");
+        }
+        
+        /// <summary>
+        /// Calling this routine ensures that the currently running application (with the provided applicationId)
+        /// is the only one running. If another application with the same unique id is already running, 
+        /// this application will exit.
+        /// </summary>
+        /// <param name="uniqueApplicationId"></param>
+        public static void EnsureSingleInstance(string uniqueApplicationId)
+        {
+            bool createdNew;
+
+            Mutex mutex = new Mutex(
+                true /* intially owned */,
+                uniqueApplicationId,
+                out createdNew);
+
+            if (!createdNew)
+            {
+                // If this app was not the one that created the mutex, 
+                // another instance of this app was already running. 
+                // Exit silently.
+                Console.WriteLine("App already running on this machine.");
+                Environment.Exit(0);
+            }
+
+            // Let the mutext not be GC'ed away.
+            GC.KeepAlive(mutex);
+        }
+        
+        /// <summary>
+        /// A background worker here is defined as an executable that doesnt consume up a cmd window or a console.
+        /// Its precesence is available for inspection only in the proc list.
+        /// </summary>
+        public static void EnsureBackgroundWorker()
+        {
+            if (Environment.GetEnvironmentVariable(UniqueBackgroundEnvName) != UniqueBackgroundEnvValue)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(CurrentApplication.GetCurrentApplication());
+                startInfo.EnvironmentVariables.Add(UniqueBackgroundEnvName, UniqueBackgroundEnvValue);
+                startInfo.CreateNoWindow = true;
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.UseShellExecute = false;
+
+                // start the background process and exit
+                Process.Start(startInfo);
+
+                Environment.Exit(0);
+            }
+        }
+
+        private static string GetCurrentApplication()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly().Location;
+        }
+    }
+}
---
+++ ExecutionStatusHelper.cs
@@ -0,0 +1,51 @@
+﻿
+namespace KeepAlive
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    /*
+     * This setting is per thread/application not global, so if you go to ES_CONTINUOUS 
+     * and another app/thread is still setting ES_DISPLAY the display will be kept on.
+     * Setting a flag without also using ES_CONTINUOUS merely resets the idle timer, 
+     * so it does not prevent sleep for an extended period.
+    */
+    class ExecutionStatusHelper
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+
+        public static void PreventMonitorPowerdown()
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        public static void AllowMonitorPowerdown()
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        public static void PreventSleep()
+        {
+            // Prevent Idle-to-Sleep (monitor not affected) (see note above)
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS | EXECUTION_STATE.ES_AWAYMODE_REQUIRED);
+        }
+
+        public static void KeepSystemAwake()
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_SYSTEM_REQUIRED);
+        }
+
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+    }
+}
